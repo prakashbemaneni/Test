048adfbadbe2112da4667d5d6e8b24dffae2ecaa
prakashbemaneni <119776229+prakashbemaneni@users.noreply.github.com>
2023-07-24 23:35:29 +0530

    Update MyController.java

--- MyController.java
+++ MyController.java
@@ -1,1 +1,17 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@RestController
+public class MyController {
+
+    @GetMapping("/public/hello")
+    public Mono<String> publicHello() {
+        return Mono.just("Hello from public endpoint!");
+    }
+
+    @GetMapping("/secure/hello")
+    public Mono<String> secureHello() {
+        return Mono.just("Hello from secure endpoint!");
+    }
+}
