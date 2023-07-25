3e3de5743f46e993e3293384d23f0267a4ff47a3
prakashbemaneni <119776229+prakashbemaneni@users.noreply.github.com>
2023-07-25 18:53:37 +0530

    Update JwtAuthenticationFilter.java

--- JwtAuthenticationFilter.java
+++ JwtAuthenticationFilter.java
@@ -1,42 +1,38 @@
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
-import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-public class JwtAuthenticationFilter extends AuthenticationWebFilter {
-    private final ReactiveAuthenticationManager authenticationManager;
+public class JwtAuthenticationFilter extends AbstractAuthenticationWebFilter {
 
-    public JwtAuthenticationFilter(ReactiveAuthenticationManager authenticationManager) {
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(ReactiveAuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         super(authenticationManager);
-        this.authenticationManager = authenticationManager;
-        setServerAuthenticationConverter(new JwtAuthenticationConverter());
-        setRequiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/secure/**"));
+        this.jwtUtil = jwtUtil;
     }
 
-    private static class JwtAuthenticationConverter implements ServerAuthenticationConverter {
-        @Override
-        public Mono<Authentication> convert(ServerWebExchange exchange) {
-            String token = extractTokenFromHeader(exchange.getRequest().getHeaders().getFirst("Authorization"));
-            if (token != null && JwtUtil.validateToken(token)) {
-                String username = JwtUtil.extractUsername(token);
-                UserDetails userDetails = User.withUsername(username).roles("USER").build();
-                return Mono.just(new UsernamePasswordAuthenticationToken(userDetails, token, userDetails.getAuthorities()));
-            }
-            return Mono.empty();
+    @Override
+    public Mono<Authentication> filter(ServerWebExchange exchange, AuthenticationFilterChain chain) {
+        String token = extractTokenFromHeader(exchange.getRequest().getHeaders().getFirst("Authorization"));
+        if (token != null && jwtUtil.validateToken(token)) {
+            String username = jwtUtil.extractUsername(token);
+            UserDetails userDetails = User.withUsername(username).roles("USER").build();
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, token, userDetails.getAuthorities());
+            return getAuthenticationManager().authenticate(authentication);
+        } else {
+            return chain.filter(exchange);
         }
+    }
 
-        private String extractTokenFromHeader(String header) {
-            if (header != null && header.startsWith("Bearer ")) {
-                return header.substring(7);
-            }
-            return null;
+    private String extractTokenFromHeader(String header) {
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7);
         }
+        return null;
     }
 }
